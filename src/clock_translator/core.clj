940c59a7ea9f5b03de9ce2341d7f14d6e4f09842
Alex Smith Torres <C17722@CHN-LT-C17722.mchp-main.com>
2017-07-06 17:44:58 -0700

    finished translator

--- src/clock_translator/core.clj
+++ src/clock_translator/core.clj
@@ -1,6 +1,41 @@
 (ns clock-translator.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def minute_words
+  ["" "one" "two" "three" "four" "five"
+   "six" "seven" "eight" "nine" "ten"
+   "eleven" "twelve" "thirteen" "fourteen" "fifteen"
+   "sixteen" "seventeen" "eighteen" "nineteen"])
+
+(def tens_words
+  ["o'" "" "twenty" "thirty" "fourty" "fifty"])
+
+(def hour_words
+  ["" "one" "two" "three" "four" "five"
+   "six" "seven" "eight" "nine" "ten"
+   "eleven" "twelve"])
+
+(defn clock-translator
+  "Translates input time in the HH:MM format to a sentance"
+  [hhmm]
+  (def timeInWords {:hours ""
+                    :tens ""
+                    :minutes ""})
+  (let [[hh mm] (map #(Integer/parseInt %) (re-seq #"\d+" hhmm))]
+    (str "It is "
+         (if (and (= hh 0) (= mm 0))
+           "midnight"
+           (str (hour_words (inc (rem (dec hh) 12)))
+                (cond
+                  (= mm 0) ""
+                  (< mm 10) " o'"
+                  (> mm 20) (str " " (tens_words (quot mm 10))))
+                (cond
+                  (= mm 0) ""
+                  (< mm 20) (minute_words mm)
+                  (> mm 20) (if (not= (rem mm 10) 0)
+                              (str " " (minute_words (rem mm 10)))))
+                (if (< hh 12)
+                  " am"
+                  " pm"))))))
+
+(map #'clock-translator '("00:00" "01:30" "12:05" "14:01" "20:29" "21:00"))
